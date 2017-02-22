c44b2303c1752c5e4fae54b89719a73459bd1cdf
Mathias Rav <rav@cs.au.dk>
2017-02-22 17:27:54 -0500

    WIP

--- bgfittf.py
+++ bgfittf.py
@@ -27,7 +27,8 @@ def scipy_optimizer(freq_filt, powerden_filt, z0):
     return popt
 
 
-def tensorflow_optimizer(freq_filt, powerden_filt, z0, learning_rate=2e-4, epochs=1000, batch_size=1024, plot_cb=None):
+def tensorflow_optimizer(freq_filt, powerden_filt, z0, learning_rate=1e-4, epochs=1000, batch_size=1024, plot_cb=None):
+    tau_limit = 0.5e-3
     with tf.Graph().as_default():
         freq = tf.placeholder(tf.float32, (None,), 'freq')
         powerden = tf.placeholder(tf.float32, (None,), 'powerden')
@@ -35,7 +36,9 @@ def tensorflow_optimizer(freq_filt, powerden_filt, z0, learning_rate=2e-4, epoch
         tau_0 = tf.Variable(tf.constant(z0[1], tf.float32))
         sigma_1 = tf.Variable(tf.constant(z0[2], tf.float32))
         tau_1 = tf.Variable(tf.constant(z0[3], tf.float32))
-        bgfit = background_fit(freq, sigma_0, tau_0, sigma_1, tau_1)
+        bgfit = background_fit(
+            freq, sigma_0, tf.maximum(tau_limit, tau_0),
+            sigma_1, tf.maximum(tau_limit, tau_1))
         log_bgfit = tf.log(bgfit)
         log_powerden = tf.log(powerden)
 
@@ -44,11 +47,10 @@ def tensorflow_optimizer(freq_filt, powerden_filt, z0, learning_rate=2e-4, epoch
 
         # Regularization: Don't let tau be too close to 0.
         tau_penalty_factor = 1e6
-        tau_penalty_limit = 0.5e-3
         tau_penalty = (
             tau_penalty_factor *
-            (tf.maximum(0.0, tau_penalty_limit - tau_0) +
-             tf.maximum(0.0, tau_penalty_limit - tau_1)))
+            (tf.maximum(0.0, tau_limit - tau_0) +
+             tf.maximum(0.0, tau_limit - tau_1)))
 
         minimization_goal = error + tau_penalty
         minimizer = tf.train.AdamOptimizer(learning_rate)
@@ -88,11 +90,13 @@ def plotter(freq_filt, powerden_filt, npoints=1000):
     plt.ion()
     fig = plt.figure()  # type: plt.Figure
     ax = fig.add_subplot(111)  # type: plt.Axes
-    data_line = ax.plot(freq_filt[ind], powerden_filt[ind], ',')  # type: plt.Line2D
-    model_line = ax.plot(xs, ys)  # type: plt.Line2D
+    data_line, = ax.plot(freq_filt[ind], np.log10(powerden_filt[ind]), ',')  # type: plt.Line2D
+    model_line, = ax.plot(xs, np.log10(ys))  # type: plt.Line2D
+    plt.pause(1e-3)
 
     def plot_cb(freq_filt, powerden_filt, bgfit, epoch, e, params):
-        model_line.set_ydata(bgfit(xs))
+        model_line.set_ydata(np.log10(bgfit(xs)))
+        plt.pause(1e-3)
 
     return plot_cb
 
