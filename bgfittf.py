fea15de0202d7b5eb49113960b5dd80d0c61fbc9
Mathias Rav <rav@cs.au.dk>
2017-02-22 22:13:08 -0500

    WIP

--- bgfittf.py
+++ bgfittf.py
@@ -40,7 +40,10 @@ def display_params(params):
     return ' '.join('%s=%.3e' % kv for kv in kvs)
 
 
-def tensorflow_optimizer(freq_filt, powerden_filt, z0, learning_rate=3e-4, epochs=1000, batch_size=2**10, plot_cb=None):
+def tensorflow_optimizer(freq_data, powerden_data, z0, weights=None, learning_rate=3e-4, epochs=1000, batch_size=2**10, plot_cb=None):
+    if weights is None:
+        weights = np.ones(len(powerden_data), dtype=np.float32)
+    total_weight = np.sum(weights)
     tau_limit = 1e-6
     with tf.Graph().as_default():
         freq = tf.placeholder(tf.float32, (None,), 'freq')
@@ -49,7 +52,7 @@ def tensorflow_optimizer(freq_filt, powerden_filt, z0, learning_rate=3e-4, epoch
         tau_0 = tf.Variable(tf.constant(z0[1], tf.float32))
         sigma_1 = tf.Variable(tf.constant(z0[2], tf.float32))
         tau_1 = tf.Variable(tf.constant(z0[3], tf.float32))
-        initial_P_n = np.exp(np.mean(np.log(powerden_filt)))
+        initial_P_n = np.exp(np.mean(np.log(powerden_data)))
         P_n = tf.Variable(tf.constant(initial_P_n, tf.float32))
         # Pass max(tau_limit/2, tau) into background_fit to avoid nan
         bgfit = background_fit(
@@ -60,8 +63,9 @@ def tensorflow_optimizer(freq_filt, powerden_filt, z0, learning_rate=3e-4, epoch
         log_bgfit = tf.log(bgfit)
         log_powerden = tf.log(powerden)
 
-        # Minimize distance squared
-        error = tf.reduce_mean((log_bgfit - log_powerden) ** 2)
+        # Minimize weighted distance squared
+        error = tf.reduce_sum(tf.constant(weights) *
+                              (log_bgfit - log_powerden) ** 2) / total_weight
         # Minimize absolute distance
         #error = tf.reduce_mean(tf.abs(log_bgfit - log_powerden))
 
@@ -82,25 +86,25 @@ def tensorflow_optimizer(freq_filt, powerden_filt, z0, learning_rate=3e-4, epoch
             for epoch in range(epochs):
                 if batch_size is None:
                     # Old-fashioned batched gradient descent
-                    data = {freq: freq_filt,
-                            powerden: powerden_filt}
+                    data = {freq: freq_data,
+                            powerden: powerden_data}
                     session.run(train_step, feed_dict=data)
                 else:
                     # Mini-batch gradient descent
-                    perm = np.random.permutation(len(freq_filt))
-                    freq_perm = freq_filt[perm]
-                    powerden_perm = powerden_filt[perm]
-                    for i in range(0, len(freq_filt), batch_size):
-                        j = min(i + batch_size, len(freq_filt))
+                    perm = np.random.permutation(len(freq_data))
+                    freq_perm = freq_data[perm]
+                    powerden_perm = powerden_data[perm]
+                    for i in range(0, len(freq_data), batch_size):
+                        j = min(i + batch_size, len(freq_data))
                         data = {freq: freq_perm[i:j],
                                 powerden: powerden_perm[i:j]}
                         session.run(train_step, feed_dict=data)
-                data = {freq: freq_filt,
-                        powerden: powerden_filt}
+                data = {freq: freq_data,
+                        powerden: powerden_data}
                 err = session.run(error, feed_dict=data)
                 params = session.run([sigma_0, tau_0, sigma_1, tau_1, P_n])
                 if plot_cb:
-                    plot_cb(freq_filt, powerden_filt,
+                    plot_cb(freq_data, powerden_data,
                             lambda x: session.run(bgfit, feed_dict={freq: x}),
                             epoch, err, params)
                 t2 = time.time()
@@ -112,20 +116,20 @@ def tensorflow_optimizer(freq_filt, powerden_filt, z0, learning_rate=3e-4, epoch
             return params
 
 
-def plotter(freq_filt, powerden_filt, npoints=10000):
-    fmin, fmax = np.min(freq_filt), np.max(freq_filt)
+def plotter(freq, powerden, npoints=10000):
+    fmin, fmax = np.min(freq), np.max(freq)
     xs = np.linspace(fmin, fmax, npoints)
-    ys = np.zeros_like(xs) + np.min(powerden_filt)
-    ind = np.random.permutation(len(freq_filt))[:npoints]
+    ys = np.zeros_like(xs) + np.min(powerden)
+    ind = np.random.permutation(len(freq))[:npoints]
 
     plt.ion()
     fig = plt.figure()  # type: plt.Figure
     ax = fig.add_subplot(111)  # type: plt.Axes
-    data_line, = ax.plot(freq_filt[ind], np.log10(powerden_filt[ind]), ',')  # type: plt.Line2D
+    data_line, = ax.plot(freq[ind], np.log10(powerden[ind]), ',')  # type: plt.Line2D
     model_line, = ax.plot(xs, np.log10(ys))  # type: plt.Line2D
     plt.pause(1e-3)
 
-    def plot_cb(freq_filt, powerden_filt, bgfit, epoch, e, params):
+    def plot_cb(freq, powerden, bgfit, epoch, e, params):
         model_line.set_ydata(np.log10(bgfit(xs)))
         plt.pause(1e-3)
 
